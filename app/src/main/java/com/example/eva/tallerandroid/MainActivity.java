9b1e1a43327521a9ac3f03dde09eae5d88e36e0b
Eva Hernández <Eva Hernndez>
2017-11-18 11:47:01 +0100

    second day

--- app/src/main/java/com/example/eva/tallerandroid/MainActivity.java
+++ app/src/main/java/com/example/eva/tallerandroid/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.eva.tallerandroid;
 
+import android.content.Intent;
+import android.support.v4.view.ViewPropertyAnimatorListener;
+import android.support.v4.view.ViewPropertyAnimatorUpdateListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.time.Instant;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);     //R es un fichero java donde están todos los recursos
+
+        Button button = findViewById(R.id.button_launcher_activity);       //Busca la view del botón por id
+        button.setOnClickListener(this);
+        Log.d("Method","OnCreate();");
     }
+
+    @Override
+    public  void onClick(View w){
+        createNewActivity();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+
+    private void createNewActivity(){
+        Intent t = new Intent(this,SecondActivity.class);
+        startActivity(t);
+    }
+
 }
